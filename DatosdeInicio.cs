781dbe27bb4b400430134f77c9503d58b6822f43
varela77 <90734107+varela77@users.noreply.github.com>
2021-11-26 17:26:39 -0800

    Add files via upload

---
+++ DatosdeInicio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Tienda
+{
+    class DatosdeInicio : CreateDatabaseIfNotExists<Contexto>
+    {
+        protected override void Seed(Contexto contexto)
+        {
+            var usuarioAdmin1 = new Usuario();
+            usuarioAdmin1.Nombre = "admin";
+            usuarioAdmin1.Contrasena = "123";
+            usuarioAdmin1.EsAdmin = true;
+
+            contexto.Usuarios.Add(usuarioAdmin1);
+
+            var usuarioGerente = new Usuario();
+            usuarioGerente.Nombre = "gerente";
+            usuarioGerente.Contrasena = "456";
+            usuarioGerente.PuedeAccederFacturas = false;
+            usuarioGerente.PuedeAccederReportes = true;
+
+            contexto.Usuarios.Add(usuarioGerente);
+
+            var categoria1 = new Categoria();
+            categoria1.Descripcion = "Rock";
+            contexto.Categorias.Add(categoria1);
+
+            var categoria2 = new Categoria();
+            categoria2.Descripcion = "Jazz";
+            contexto.Categorias.Add(categoria2);
+
+            var categoria3 = new Categoria();
+            categoria3.Descripcion = "Pop";
+            contexto.Categorias.Add(categoria3);
+
+            var categoria4 = new Categoria();
+            categoria4.Descripcion = "Soul";
+            contexto.Categorias.Add(categoria4);
+
+            var tipo1 = new Tipo();
+            tipo1.Descripcion = "Reproductor";
+            contexto.Tipos.Add(tipo1);
+
+            var tipo2 = new Tipo();
+            tipo2.Descripcion = "Album";
+            contexto.Tipos.Add(tipo2);
+
+            var tipo3 = new Tipo();
+            tipo3.Descripcion = "Accesorios";
+            contexto.Tipos.Add(tipo3);
+
+            base.Seed(contexto); 
+        }
+    }
+}
---
+++ SeguridadBL.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Tienda
+{
+    public class SeguridadBL
+    {
+        Contexto _contexto;
+        public BindingList<Usuario> ListaUsuarios { get; set; }
+
+        public SeguridadBL()
+        {
+            _contexto = new Contexto();
+            ListaUsuarios = new BindingList<Usuario>();
+        }
+
+        public BindingList<Usuario> ObtenerUsuarios()
+        {
+            _contexto.Usuarios.Load();
+            ListaUsuarios = _contexto.Usuarios.Local.ToBindingList();
+
+            return ListaUsuarios;
+        }
+         
+        public Usuario Autorizar(string usuario, string contrasena)
+        {
+            var usuarios = _contexto.Usuarios.ToList();
+
+            foreach (var usuarioDB in usuarios)
+            {
+                if (usuario == usuarioDB.Nombre && contrasena == usuarioDB.Contrasena)
+                {
+                    return usuarioDB;
+                }
+            }
+                        
+            return null;
+        }
+
+        public void CancelarCambios()
+        {
+            foreach (var item in _contexto.ChangeTracker.Entries())
+            {
+                item.State = EntityState.Unchanged;
+                item.Reload();
+            }
+        }
+
+        public Resultado GuardarUsuario(Usuario usuario)
+        {
+            var resultado = Validar(usuario);
+            if (resultado.Exitoso == false)
+            {
+                return resultado;
+            }
+
+            _contexto.SaveChanges();
+
+            resultado.Exitoso = true;
+            return resultado;
+        }
+
+        public void AgregarUsuario()
+        {
+            var nuevoUsuario = new Usuario();
+            ListaUsuarios.Add(nuevoUsuario);
+        }
+
+        public bool EliminarUsuario(int id)
+        {
+            foreach (var usuario in ListaUsuarios)
+            {
+                if (usuario.Id == id)
+                {
+                    ListaUsuarios.Remove(usuario);
+                    _contexto.SaveChanges();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private Resultado Validar(Usuario usuario)
+        {
+            var resultado = new Resultado();
+            resultado.Exitoso = true;
+
+            if (usuario == null)
+            {
+                resultado.Mensaje = "Agregue un usuario válido";
+                resultado.Exitoso = false;
+
+                return resultado;
+            }
+
+            if (string.IsNullOrEmpty(usuario.Nombre) == true)
+            {
+                resultado.Mensaje = "Ingrese un nombre";
+                resultado.Exitoso = false;
+            }
+
+            if (string.IsNullOrEmpty(usuario.Contrasena) == true)
+            {
+                resultado.Mensaje = "Ingrese Contraseña";
+                resultado.Exitoso = false;
+            }
+
+            return resultado;
+        }
+    }
+
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasena { get; set; }
+
+        public bool EsAdmin { get; set; }
+        public bool PuedeAccederModelos { get; set; }
+        public bool PuedeAccederClientes { get; set; }
+        public bool PuedeAccederFacturas { get; set; }
+        public bool PuedeAccederReportes { get; set; }
+
+        public Usuario()
+        {   
+            PuedeAccederModelos = false;
+            PuedeAccederClientes = false;
+            PuedeAccederFacturas = true;
+            PuedeAccederReportes = false;
+        }
+    }
+}
